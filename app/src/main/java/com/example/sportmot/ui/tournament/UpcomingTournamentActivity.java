4d92d9d2b53927a55ccfd0d8574c7f9d9791fc8e
torhildur <72811407+torhildur@users.noreply.github.com>
2025-03-05 18:34:03 +0000

    Made a fragment for the register team form.

--- app/src/main/java/com/example/sportmot/ui/tournament/UpcomingTournamentActivity.java
+++ app/src/main/java/com/example/sportmot/ui/tournament/UpcomingTournamentActivity.java
@@ -1,12 +1,16 @@
 package com.example.sportmot.ui.tournament;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sportmot.R;
+
+import com.example.sportmot.ui.tournament.fragment.RegisterFormFragment;
+
 
 public class UpcomingTournamentActivity extends AppCompatActivity {
 
@@ -15,10 +19,23 @@ public class UpcomingTournamentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournament_list);
         Button til_baka = findViewById(R.id.til_baka);
+        Button skra_lid = findViewById(R.id.skra_lid);
         TextView tournament_title = findViewById(R.id.tournament_title);
         tournament_title.setText("Næstu mót");
         til_baka.setOnClickListener((v) ->
                 onBackPressed()
         );
+
+
+
+
+        skra_lid.setOnClickListener(v ->{
+            RegisterFormFragment fragment = new RegisterFormFragment();
+
+            findViewById(R.id.formFragment).setVisibility(View.VISIBLE);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.formFragment,fragment).addToBackStack(null).commit();
+        });
+
     }
 }
