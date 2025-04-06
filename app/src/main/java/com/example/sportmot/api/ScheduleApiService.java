5d3e4263965bb92373aec48250cbf89af4600614
torhildur <72811407+torhildur@users.noreply.github.com>
2025-04-06 18:32:50 +0000

    In Mín mót I'm getting the tournament from Challonge API because that has schedule. I also get the teams because you can't add  teams without an account.

--- app/src/main/java/com/example/sportmot/api/ScheduleApiService.java
+++ app/src/main/java/com/example/sportmot/api/ScheduleApiService.java
@@ -4,6 +4,7 @@ package com.example.sportmot.api;
 //import com.example.sportmot.data.entities.Game;
 //import com.example.sportmot.data.entities.Team;
 
+import com.example.sportmot.data.entities.ChallongeTeamWrapper;
 import com.example.sportmot.data.entities.TournamentNewWrapper;
 
 import java.util.List;
@@ -15,4 +16,12 @@ public interface ScheduleApiService {
 
     @GET("tournaments.json")
     Call<List<TournamentNewWrapper>> getTournaments(@Query("api_key") String apiKey);
+
+
+    @GET("tournaments/{tournament_id}/participants.json")
+    Call<List<ChallongeTeamWrapper>> getTeams(
+            @Path("tournament_id") String tournamentId,
+            @Query("api_key") String apiKey
+    );
+
 }
---
+++ app/src/main/java/com/example/sportmot/data/entities/ChallongeTeam.java
@@ -0,0 +1,15 @@
+package com.example.sportmot.data.entities;
+
+//Teams from Challonge
+public class ChallongeTeam {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
